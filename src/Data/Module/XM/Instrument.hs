e80c7e8076745fc38d1ed02412f3df755c5be7da
kohlrabi <kohlrabi@aragonit>
2016-05-19 13:28:34 +0200

    basic .xm instrument parsing

---
+++ src/Data/Module/XM/Instrument.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Data.Module.XM.Instrument (
+      Instrument (..)
+    , getInstrument
+    , putInstrument
+    ) where
+
+import           Data.Module.XM.Sample
+
+import           Control.Applicative
+import           Control.Monad
+import           Data.Binary
+import           Data.Word
+import           Data.Binary.Get
+import           Data.Binary.Put
+
+data Instrument = Instrument { instrumentSize :: Word64
+                             , name           :: [Word8]    -- 22 bytes
+                             , instrumentType :: Word8
+                             , numSamples     :: Word16
+-- if numSamples > 0
+-- TODO
+                             }
+    deriving (Show, Eq)
+
+getInstrument :: Get Instrument
+getInstrument = Instrument <$> getWord64le <*> replicateM 22 getWord8
+                           <*> getWord8 <*> getWord16le
+-- TODO
+
+putInstrument :: Instrument -> Put
+putInstrument Instrument{..} = do
+    putWord64le instrument
+    putM_ name
+    putWord8 instrumentType
+    putWord16le numSamples
+-- TODO
+
