ec7b6d6f13b4791cef4ffcc8d72309342538c4ad
kohlrabi <kohlrabi@aragonit>
2016-05-19 02:12:46 +0200

    Now parsing extended module headers

---
+++ src/Data/Module/XM/Header.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Data.Module.XM.Header (
+      Header (..)
+    , getHeader
+    , putHeader
+    ) where
+
+import           Control.Applicative
+import           Control.Monad
+import           Data.Binary
+import           Data.Word
+import           Data.Binary.Get
+import           Data.Binary.Put
+ 
+data Header = Header { idText          :: [Word8]    -- 17 bytes: "Extended module: "
+                     , songName        :: [Word8]    -- 20 bytes
+                     , hpad0           :: Word8
+                     , trackerName     :: [Word8]    -- 20 bytes
+                     , version         :: Word16
+                     , headerSize      :: Word64
+                     , patNum          :: Word16     -- number of patterns
+                     , restartPosition :: Word16
+                     , numChannels     :: Word16
+                     , numPatterns     :: Word16
+                     , numInstruments  :: Word16
+                     , flags           :: Word16    -- bit 0: use linear freq. table
+                     , defaultTempo    :: Word16
+                     , defaultBPM      :: Word16
+                     }
+    deriving (Show, Eq)
+
+getHeader :: Get Header
+getHeader = Header <$> replicateM 17 getWord8
+                   <*> replicateM 20 getWord8
+                   <*> getWord8
+                   <*> replicateM 20 getWord8
+                   <*> getWord16le
+                   <*> getWord64le
+                   <*> getWord16le
+                   <*> getWord16le
+                   <*> getWord16le
+                   <*> getWord16le
+                   <*> getWord16le
+                   <*> getWord16le
+                   <*> getWord16le
+                   <*> getWord16le
+
+putHeader :: Header -> Put
+putHeader Header{..} = do
+    mapM_ putWord8 (idText ++ songName ++ [hpad0] ++ trackerName)
+    putWord16le version
+    putWord64le headerSize
+    mapM_ putWord16le
+          [ patNum
+          , restartPosition
+          , numChannels
+          , numPatterns
+          , numInstruments
+          , flags
+          , defaultTempo
+          , defaultBPM
+          ]
+
