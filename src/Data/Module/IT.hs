4223c8cfbd2c6c5b8ceb9a18e6197b2244e55eab
kohlrabi <kohlrabi@aragonit>
2016-08-02 13:55:36 +0200

    IT: now reads and writes offset block following the header

--- src/Data/Module/IT.hs
+++ src/Data/Module/IT.hs
@@ -12,6 +12,8 @@ import           Data.Word
 import           Data.Binary.Get
 import           Data.Binary.Put
 
+import           Numeric (showHex)
+
 import Data.Module.IT.Header
 import Data.Module.IT.Instrument
 import Data.Module.IT.Sample
@@ -19,9 +21,12 @@ import Data.Module.IT.Pattern
 
 data Module = Module { header         :: Header
                      , orders         :: [Word8]
-                     , instruments    :: [Instrument]
-                     , sampleHeaders  :: [SampleHeader]
-                     , patterns       :: [Pattern]
+                     , insOffsets     :: [Word32]
+                     , smpOffsets     :: [Word32]
+                     , patOffsets     :: [Word32]
+--                     , instruments    :: [Instrument]
+--                     , sampleHeaders  :: [SampleHeader]
+--                     , patterns       :: [Pattern]
                      }
     deriving (Show, Eq)
 
@@ -29,16 +34,22 @@ getModule :: Get Module
 getModule = do
     header <- getHeader
     orders <- replicateM (fromIntegral (ordNum header))  getWord8
-    instruments <- replicateM (fromIntegral (insNum header)) getInstrument
-    sampleHeaders <- replicateM (fromIntegral (smpNum header)) getSampleHeader
-    patterns <- replicateM (fromIntegral (patNum header)) getPattern
+    insOffsets <- replicateM (fromIntegral (insNum header)) getWord32le
+    smpOffsets <- replicateM (fromIntegral (smpNum header)) getWord32le
+    patOffsets <- replicateM (fromIntegral (patNum header)) getWord32le
+--    instruments <- replicateM (fromIntegral (insNum header)) getInstrument
+--    sampleHeaders <- replicateM (fromIntegral (smpNum header)) getSampleHeader
+--    patterns <- replicateM (fromIntegral (patNum header)) getPattern
     return $ Module{..}
 
 putModule :: Module -> Put
 putModule Module{..} = do
     putHeader header
     mapM_ putWord8 orders
-    mapM_ putInstrument instruments
-    mapM_ putSampleHeader sampleHeaders
-    mapM_ putPattern patterns
+    mapM_ putWord32le insOffsets
+    mapM_ putWord32le smpOffsets
+    mapM_ putWord32le patOffsets
+--    mapM_ putInstrument instruments
+--    mapM_ putSampleHeader sampleHeaders
+--    mapM_ putPattern patterns
 
