5bb35afcc0e578d7c25c8522bd4f5104975a2f03
kohlrabi <kohlrabi@aragonit>
2016-08-04 00:12:27 +0200

    IT: more convenient example

--- examples/readIT.hs
+++ examples/readIT.hs
@@ -1,12 +1,44 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Main (main) where
 
+import Control.Monad
+
 import Data.Module.IT
+import Data.Module.IT.Instrument
+import Data.Module.IT.Header
 
 import Data.Binary.Get
 
 import qualified Data.ByteString.Lazy as BL
 
+pprintInstrument :: Instrument -> IO ()
+pprintInstrument Instrument{..} = do
+    putStr $ "(" ++ show globalVolume ++ "): "
+    BL.putStrLn $ BL.pack name
+
+pprintHeader :: Header -> IO ()
+pprintHeader Header{..} = do
+    putStr     "Song name....: "
+    BL.putStrLn $ BL.pack songName
+    putStrLn $ "Orders.......: " ++ show ordNum
+    putStrLn $ "Instruments..: " ++ show insNum
+    putStrLn $ "Samples......: " ++ show smpNum
+    putStrLn $ "Patterns.....: " ++ show patNum
+    putStrLn $ "Global volume: " ++ show globalVolume
+    putStrLn $ "Mix volume...: " ++ show mixVolume
+    putStrLn $ "Initial speed: " ++ show initialSpeed
+    putStrLn $ "Initial tempo: " ++ show initialTempo
+
 main :: IO ()
 main = do
     file <- BL.getContents
-    print $ runGet getModule file
+    let it = runGet getModule file
+    putStrLn "Header:"
+    putStrLn "======="
+    pprintHeader $ header it
+    putStrLn "<>"
+    putStrLn "Instruments:"
+    putStrLn "============"
+    mapM_ pprintInstrument (instruments it)
+    putStrLn "<>"
