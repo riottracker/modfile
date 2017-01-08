804275fa2913d4d8e40bf11e8d95ce1f20f4a1d9
kohlrabi <kohlrabi@aragonit>
2017-01-08 22:11:03 +0100

    s3m: read instruments and patterns

--- src/Data/Module/S3M.hs
+++ src/Data/Module/S3M.hs
@@ -12,16 +12,22 @@ import           Data.Word
 import           Data.Binary.Get
 import           Data.Binary.Put
 
-
-import Data.Module.S3M.Header
+import           Data.Module.S3M.Header
+import           Data.Module.S3M.Instrument
+import           Data.Module.S3M.Pattern
 
 data Module = Module { header      :: Header
                      , orders      :: [Word8]
                      , insOffsets  :: [Word16]
                      , patOffsets  :: [Word16]
--- OPTIONAL:         , panning     :: [Word8]    
+-- OPTIONAL:         , panning     :: [Word8]
+                     , instruments :: [Instrument]
+                     , patterns    :: [Pattern]
                      }
     deriving (Show, Eq)
+
+getAtOffset :: Get a -> Word16 -> Get a
+getAtOffset f n = lookAhead $ (>> f) . skip . (-) (fromIntegral n) . fromIntegral =<< bytesRead
 
 getModule :: Get Module
 getModule = do
@@ -29,6 +35,8 @@ getModule = do
     orders <- replicateM (fromIntegral (numOrders header)) getWord8
     insOffsets <- replicateM (fromIntegral (numInstruments header)) getWord16le
     patOffsets <- replicateM (fromIntegral (numPatterns header)) getWord16le
+    instruments <- mapM (getAtOffset getInstrument) insOffsets
+    patterns <- mapM (getAtOffset getPattern) patOffsets
     return $ Module{..}
 
 putModule :: Module -> Put
@@ -37,4 +45,5 @@ putModule Module{..} = do
     mapM_ putWord8 orders
     mapM_ putWord16le insOffsets
     mapM_ putWord16le patOffsets
+    fail "todo: write instruments and patterns"
 
